20e2172b53cb1abe4529c9acc003ff1d189cbb96
Wajid Abbas <92284984+waaj529@users.noreply.github.com>
2025-08-09 02:03:22 +0500

    Remove Most Popular badge from 4-Hour Adventure

--- components/tour-options-section.tsx
+++ components/tour-options-section.tsx
@@ -59,7 +59,7 @@ export default function TourOptionsSection() {
         'Professional captain',
         'Safety equipment included'
       ],
-      popular: true
+      popular: false
     }
   ];
 
